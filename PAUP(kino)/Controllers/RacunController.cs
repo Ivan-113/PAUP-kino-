aada7ec0b37bf97558002277ed9ac9bc6f893e8d
Ivan <androic.ivan@gmail.com>
2020-04-17 17:51:05 +0200

    Paup-kino

---
+++ PAUP(kino)/Controllers/RacunController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PAUP_kino_.Models;
+using System.Data.SqlClient;
+
+namespace PAUP_kino_.Controllers
+{
+    public class RacunController : Controller
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Racun
+        [HttpGet]
+
+      public ActionResult Index() 
+        {
+            return View();
+        }
+        public ActionResult Prijava()
+        {
+            return View();
+        }
+
+        void povezivanjeSBazom() 
+        {
+            con.ConnectionString = "data source=potrebno unijeti source baze;database=WPF; integrated security=SSPI;";
+        
+        }
+        [HttpPost]
+        public ActionResult Potvrda(Racun rac)
+        {
+            povezivanjeSBazom();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Potrebno unijeti putanju baze ";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("UspjesnaPrijava");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+      
+           
+        }
+
+       
+    }
+}
---
+++ PAUP(kino)/Models/Racun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAUP_kino_.Models
+{
+    public class Racun
+    {
+        public string Ime { get; set; }
+        public string Sifra { get; set; }
+
+    }
+}
